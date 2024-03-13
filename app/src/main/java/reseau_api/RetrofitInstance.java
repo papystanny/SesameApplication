6ac873cd883a3a432227136f22176b698bbeb7a6
ScribeQc <93779862+ScribeQc@users.noreply.github.com>
2024-03-13 13:55:53 -0400

    db con adaptation

--- app/src/main/java/reseau_api/RetrofitInstance.java
+++ app/src/main/java/reseau_api/RetrofitInstance.java
@@ -15,7 +15,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
-    public static final String BASE_URL = "http://10.0.2.2:8000";
+    public static final String BASE_URL = "http://172.16.207.146:8000";
+
 
     private static Retrofit retrofit;
 
